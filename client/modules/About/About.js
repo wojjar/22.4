f02c1a07c3c639c23c8d295e1e226fee8d0e4914
wojjar <jaroslaw@wojcik.hub.pl>
2018-03-30 17:32:34 +0200

    Update about.js

--- client/modules/About/About.js
+++ client/modules/About/About.js
@@ -2,14 +2,11 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-
-class About extends Component {
-  render() {
+ const About = () => { 
     return (
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel posuere nulla. Fusce lobortis, purus quis laoreet luctus, enim libero feugiat elit, in feugiat neque leo non ipsum. Ut et aliquam purus, sit amet condimentum leo. Sed nec nunc orci. 
         Nulla metus lectus, maximus posuere pretium ac, placerat et nulla.</p>
-    );
-  }
+    ); 
 }
 
 const mapStateToProps = (state) => {
